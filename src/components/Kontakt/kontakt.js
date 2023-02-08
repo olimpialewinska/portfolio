b99d16dfe83db065228a22373a1057ccb08d3435
Olimpia <o.lewinska.336@studms.ug.edu.pl>
2023-02-08 15:08:57 +0100

    login

--- src/components/Kontakt/kontakt.js
+++ src/components/Kontakt/kontakt.js
@@ -1,21 +1,36 @@
 import React from "react";
 
+
 function Kontakt() {
   return (
     <div className="container">
+      <div className="bg">
+      
+      </div>
       <hr className="hr"></hr>
       <div align="center" className="socialbtns">
         <ul>
           <li>
-            <a href="https://www.facebook.com/oskarlewinskifotografia" target="_blank" rel="noreferrer"  className="fa fa-lg fa-facebook"></a>
+            <a
+              href="https://www.facebook.com/oskarlewinskifotografia"
+              target="_blank"
+              rel="noreferrer"
+            ><div className="fa fa-lg fa-facebook"></div></a>
           </li>
           <li>
-            <a href="https://www.instagram.com/oskarlewinski/"  target="_blank" rel="noreferrer" className="fa fa-lg fa-instagram"></a>
+            <a
+              href="https://www.instagram.com/oskarlewinski/"
+              target="_blank"
+              rel="noreferrer"
+            ><div ></div></a>
           </li>
           <li>
-            <a href="https://www.facebook.com/lewinskiphotografia"  target="_blank" rel="noreferrer" className="fa fa-lg fa-facebook"></a>
+            <a
+              href="https://www.facebook.com/lewinskiphotografia"
+              target="_blank"
+              rel="noreferrer"
+            ><div className="fa fa-lg fa-facebook"></div></a>
           </li>
-         
         </ul>
       </div>
     </div>
